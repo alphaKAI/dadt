234072ab42611536c90cb9ce6d3f8bd7b176581b
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2018-08-02 23:03:41 +0900

    update stdadts/binarytree,either

--- source/dadt/stdadts/binarytree.d
+++ source/dadt/stdadts/binarytree.d
@@ -1,4 +1,5 @@
 module source.dadt.stdadts.binarytree;
+
 enum BinaryTreeType {
   Node,
   Leaf
@@ -109,4 +110,75 @@ _RETURN_TYPE_OF_MATCH_WITH_BinaryTree matchWithBinaryTree(
   static if (!is(_RETURN_TYPE_OF_MATCH_WITH_BinaryTree == void)) {
     return null;
   }
+}
+
+string show_BinaryTree(T)(BinaryTree!(T) arg, string function(BinaryTree!(T)) conv = null) {
+  if (conv !is null) {
+    return conv(arg);
+  }
+  import std.string;
+
+  string[] interface_args = [T.stringof];
+  string constructor_arg;
+  if (interface_args.length) {
+    constructor_arg = "!(" ~ interface_args.join(", ") ~ ")";
+  }
+
+  final switch (arg.type) {
+  case BinaryTreeType.Node:
+    Node!(T) x = cast(Node!(T))arg;
+
+    return "Node" ~ constructor_arg ~ "(%s, %s)".format(x._0, x._1);
+  case BinaryTreeType.Leaf:
+    Leaf!(T) x = cast(Leaf!(T))arg;
+
+    return "Leaf" ~ constructor_arg ~ "(%s)".format(x._0);
+  }
+}
+
+int compare_BinaryTree(T)(BinaryTree!(T) _lhs, BinaryTree!(T) _rhs) {
+
+  BinaryTreeType lhs_type = _lhs.type, rhs_type = _rhs.type;
+
+  if (lhs_type < rhs_type) {
+    return -1;
+  }
+  if (lhs_type > rhs_type) {
+    return 1;
+  }
+
+  final switch (lhs_type) {
+  case BinaryTreeType.Node:
+    Node!(T) lhs = cast(Node!(T))_lhs, rhs = cast(Node!(T))_rhs;
+
+    if (lhs._0 < rhs._0) {
+      return -1;
+    }
+    if (lhs._0 > rhs._0) {
+      return 1;
+    }
+    if (lhs._1 < rhs._1) {
+      return -1;
+    }
+    if (lhs._1 > rhs._1) {
+      return 1;
+    }
+
+    return 0;
+  case BinaryTreeType.Leaf:
+    Leaf!(T) lhs = cast(Leaf!(T))_lhs, rhs = cast(Leaf!(T))_rhs;
+
+    if (lhs._0 < rhs._0) {
+      return -1;
+    }
+    if (lhs._0 > rhs._0) {
+      return 1;
+    }
+
+    return 0;
+  }
+}
+
+bool equal_BinaryTree(T)(BinaryTree!(T) _lhs, BinaryTree!(T) _rhs) {
+  return compare_BinaryTree!(T)(_lhs, _rhs) == 0;
 }
--- source/dadt/stdadts/either.d
+++ source/dadt/stdadts/either.d
@@ -108,3 +108,68 @@ _RETURN_TYPE_OF_MATCH_WITH_Either matchWithEither(
     return null;
   }
 }
+
+string show_Either(T, U)(Either!(T, U) arg, string function(Either!(T, U)) conv = null) {
+  if (conv !is null) {
+    return conv(arg);
+  }
+  import std.string;
+
+  string[] interface_args = [T.stringof, U.stringof];
+  string constructor_arg;
+  if (interface_args.length) {
+    constructor_arg = "!(" ~ interface_args.join(", ") ~ ")";
+  }
+
+  final switch (arg.type) {
+  case EitherType.Right:
+    Right!(T, U) x = cast(Right!(T, U))arg;
+
+    return "Right" ~ constructor_arg ~ "(%s)".format(x._0);
+  case EitherType.Left:
+    Left!(T, U) x = cast(Left!(T, U))arg;
+
+    return "Left" ~ constructor_arg ~ "(%s)".format(x._0);
+  }
+}
+
+int compare_Either(T, U)(Either!(T, U) _lhs, Either!(T, U) _rhs) {
+
+  EitherType lhs_type = _lhs.type, rhs_type = _rhs.type;
+
+  if (lhs_type < rhs_type) {
+    return -1;
+  }
+  if (lhs_type > rhs_type) {
+    return 1;
+  }
+
+  final switch (lhs_type) {
+  case EitherType.Right:
+    Right!(T, U) lhs = cast(Right!(T, U))_lhs, rhs = cast(Right!(T, U))_rhs;
+
+    if (lhs._0 < rhs._0) {
+      return -1;
+    }
+    if (lhs._0 > rhs._0) {
+      return 1;
+    }
+
+    return 0;
+  case EitherType.Left:
+    Left!(T, U) lhs = cast(Left!(T, U))_lhs, rhs = cast(Left!(T, U))_rhs;
+
+    if (lhs._0 < rhs._0) {
+      return -1;
+    }
+    if (lhs._0 > rhs._0) {
+      return 1;
+    }
+
+    return 0;
+  }
+}
+
+bool equal_Either(T, U)(Either!(T, U) _lhs, Either!(T, U) _rhs) {
+  return compare_Either!(T, U)(_lhs, _rhs) == 0;
+}
